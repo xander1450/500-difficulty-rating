18a487c516d6cf0da1c3104e8c2b6fd1b34179b8
Aditya Narayan Verma <adityaxxx2003@gmail.com>
2026-02-08 16:28:00 +0530

    Create Speciality.py

---
+++ Speciality.py
@@ -0,0 +1,8 @@
+for i in range(int(input())):
+    x,y,z=map(int, input().split())
+    if(x>y and x>z):
+        print("Setter")
+    elif(y>x and y>z):
+        print("Tester")
+    else:
+        print("Editorialist")
